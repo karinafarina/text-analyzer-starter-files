7c2d22bdb5e03b04387be13d11cb78ae282c8524
Karina Gaulin <karinagaulin@gmail.com>
2017-02-16 10:28:07 -0500

    Inital Commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// your code here!
+
+$(document).ready(function() { 
+	$('form').submit(function(event) {
+		console.log("Hello");
+	event.preventDefault();
+	var wordCount = $('#user-text').val().split(' ');
+	alert(wordCount.length);
+	$("#numWords").text(wordCount.length);
+	})
+});
+
+
+
+
+
+
+
+
